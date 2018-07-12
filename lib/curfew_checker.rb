ef5a4e486f75047e1c856b9e759bcfc5b127d3cd
melodytng <melodytang2002@yahoo.com>
2018-07-12 15:58:59 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,24 +1,24 @@
-# def simple_curfew_checker(time)
-#   if time < 11
-#     puts "#{time} is less than 11"
-# end
+def simple_curfew_checker(time)
+  if time < 11
+    puts "#{time} is less than 11"
+end
 
-# def curfew_checker(time)
-#   if time < 11
-#     puts "#{time} is less than 11"
-#   else
-#     puts "You're in trouble! Better get back to Hogwarts quick!"
-# end
+def curfew_checker(time)
+  if time < 11
+    puts "#{time} is less than 11"
+  else
+    puts "You're in trouble! Better get back to Hogwarts quick!"
+end
 
-# def complex_curfew_checker(time)
-#   if time < 11
-#     puts "#{time} is less than 11"
-#   elsif time == 11
-#     puts "#{time} equals 11"
-#   else
-#     puts "You're in trouble! Better get back to Hogwarts quick!"
-#   end
-# end
+def complex_curfew_checker(time)
+  if time < 11
+    puts "#{time} is less than 11"
+  elsif time == 11
+    puts "#{time} equals 11"
+  else
+    puts "You're in trouble! Better get back to Hogwarts quick!"
+  end
+end
 
 def deluxe_curfew_checker(time)
   curfew = 11
@@ -31,12 +31,12 @@ def deluxe_curfew_checker(time)
 end
 puts deluxe_curfew_checker(10)
 
-# def platinum_curfew_checker(current_time, curfew_time)
-#   time_left = curfew_time - current_time
-#   if time_left > 0 
-#     puts "You have #{time_left} hour(s) left to keep having fun!"
-#   else
-#     puts "You're in trouble! Better get back to Hogwarts quick!"
-#   end
-# end
+def platinum_curfew_checker(current_time, curfew_time)
+  time_left = curfew_time - current_time
+  if time_left > 0 
+    puts "You have #{time_left} hour(s) left to keep having fun!"
+  else
+    puts "You're in trouble! Better get back to Hogwarts quick!"
+  end
+end
 
